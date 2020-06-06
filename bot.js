68ae502b809eebe455e6ec34d0684ce389e7da26
Tobias Rös <roes@amicaldo.de>
2020-06-07 00:22:14 +0200

    this commit adds just "missing" ; to the end of a line

--- bot.js
+++ bot.js
@@ -6,29 +6,29 @@ var ci = require('correcting-interval');
 var moment = require('moment');
 var Axios = require('axios');
 const current = new Store({ path: 'data/current.json' });
-var Hook = []
+var Hook = [];
 config.discord.webhookURL.forEach(function(webhk){
   Hook.push(new Discord.WebhookClient(webhk.id, webhk.token))
-})
+});
 ci.setCorrectingInterval(function() {  
   	ids.forEach(function(user){
-  		var id = user.replace(/\r?\n|\r/g, "")
+  		var id = user.replace(/\r?\n|\r/g, "");
   		Axios.get('https://www.instagram.com/'+id+'/').then(data => {
-	  		const jsonObject = data.data.match(/<script type="text\/javascript">window\._sharedData = (.*)<\/script>/)[1].slice(0, -1)
-	  		var info = JSON.parse(jsonObject)
+	  		const jsonObject = data.data.match(/<script type="text\/javascript">window\._sharedData = (.*)<\/script>/)[1].slice(0, -1);
+	  		var info = JSON.parse(jsonObject);
 	  		if(info.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges[0]!==undefined){
-		  		var latest = info.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges[0].node
+		  		var latest = info.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges[0].node;
 		  		if(latest!==undefined){
-		  			var caption = 'No caption.'
+		  			var caption = 'No caption.';
 		  			if(latest.edge_media_to_caption.edges[0]!==undefined){caption=latest.edge_media_to_caption.edges[0].node.text}
 			  		if(current.hasOwn(id)===true){
-			  			var curid = current.get(id)
+			  			var curid = current.get(id);
 			  			if(curid!==latest.id){
-			  				current.set(id, latest.id)
+			  				current.set(id, latest.id);
 			  				postToWebhook(id, 'https://www.instagram.com/'+id+'/', latest.display_url, caption)
 			  			}
 			  		}else{
-			  			current.set(id, latest.id)
+			  			current.set(id, latest.id);
 			  			postToWebhook(id, 'https://www.instagram.com/'+id+'/', latest.display_url, caption)
 			  		}
 			  	}
